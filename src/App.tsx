b468b69946d7e69b6e2e1d74864214ec103f5034
Volodya Taras <proger.sdk@gmail.com>
2020-08-06 15:56:26 +0300

    Add ThemeProvider, structure of homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './App.css'
 import Homepage from './components/homepage'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    background: {
+      backgroundColor: theme.palette.background.default
+    }
+  })
+)
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
+    <div className={`app ${classes.background}`}>
       <Homepage />
     </div>
   )
--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,8 +1,62 @@
 import React from 'react'
 import Layout from '../layout'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
 
-const Homepage = () => {
-  return <Layout>Homepage</Layout>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2)
+      // color: theme.palette.text.secondary
+    },
+    grid: {
+      paddingTop: theme.spacing(3)
+    }
+  })
+)
+
+export default function Homepage() {
+  const classes = useStyles()
+
+  return (
+    <Layout>
+      <div className={classes.root}>
+        <Container maxWidth="md">
+          <Grid
+            container
+            spacing={3}
+            direction="column"
+            alignItems="stretch"
+            className={classes.grid}
+          >
+            <ArticleLink />
+            <ArticleLink />
+            <ArticleLink />
+            <ArticleLink />
+            <ArticleLink />
+          </Grid>
+        </Container>
+      </div>
+    </Layout>
+  )
 }
 
-export default Homepage
+const ArticleLink = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <Grid item>
+        <Paper className={classes.paper} elevation={3}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          illum beatae quis illo fugiat ratione!
+        </Paper>
+      </Grid>
+    </>
+  )
+}
