f4d9d12c9268d71c536a9def88cf6a775846da20
Volodya Taras <proger.sdk@gmail.com>
2020-08-06 12:21:09 +0300

    Format files

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme: Theme) =>
-createStyles({
+  createStyles({
     root: {
-      flexGrow: 1,
+      flexGrow: 1
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+      marginRight: theme.spacing(2)
     },
     title: {
-      flexGrow: 1,
-    },
-  }),
-);
+      flexGrow: 1
+    }
+  })
+)
 
 const Header = () => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
@@ -33,7 +33,7 @@ const Header = () => {
         </Toolbar>
       </AppBar>
     </div>
-  );
+  )
 }
 
-  export default Header
+export default Header
--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import Layout from '../layout'
 
 const Homepage = () => {
-  return (
-    <Layout>Homepage</Layout>
-  )
+  return <Layout>Homepage</Layout>
 }
 
 export default Homepage
