f5abd4491c4ae72d12754ae3d31b0ab31e95ddf9
Volodya Taras <proger.sdk@gmail.com>
2020-08-13 19:42:35 +0300

    Modify api, add firebase

---
+++ src/api/articles.ts
@@ -0,0 +1,47 @@
+const url = 'https://api.nytimes.com/'
+const apiKey = process.env.REACT_APP_API_KEY_TIMES
+
+let TopStoriesSection:
+  | 'arts'
+  | 'automobiles'
+  | 'books'
+  | 'business'
+  | 'food'
+  | 'health'
+  | 'home'
+  | 'insider'
+  | 'magazine'
+  | 'movies'
+  | 'opinion'
+  | 'politics'
+  | 'science'
+  | 'sports'
+  | 'technology'
+  | 'travel'
+  | 'upshot'
+  | 'us'
+  | 'world'
+
+export type TopStoriesSection = typeof TopStoriesSection
+
+export const articlesAPI = {
+  getTopStories: async (section: TopStoriesSection) => {
+    let response = await fetch(
+      `${url}svc/topstories/v2/${section}.json?api-key=${apiKey}`
+    )
+    let result = await response.json()
+
+    if (response.status === 401) {
+      result = {
+        message: 'Unauthorized request. Make sure api-key is set.'
+      }
+    } else if (response.status === 429) {
+      result = {
+        message:
+          'Too many requests. You reached your per minute or per day rate limit.'
+      }
+    }
+
+    return result
+  }
+}
---
+++ src/api/firebase.ts
@@ -0,0 +1,24 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+}
+
+app.initializeApp(config)
+const auth = app.auth()
+
+export const firebaseAPI = {
+  createUserWithEmailAndPassword: (email: string, password: string) =>
+    auth.createUserWithEmailAndPassword(email, password),
+
+  signInWithEmailAndPassword: (email: string, password: string) =>
+    auth.signInWithEmailAndPassword(email, password),
+
+  signOut: () => auth.signOut()
+}
